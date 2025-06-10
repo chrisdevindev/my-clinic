49d3d870cc905ef07b1734e04fd9231b02a9baaf
ccastro01 <alvescastro00cca13@gmail.com>
2025-06-10 00:25:54 -0300

    feat: add clinic data to user session

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { customSession } from "better-auth/plugins"; // Adjust the import path if customSession is defined elsewhere
+import { eq } from "drizzle-orm";
 
 import { db } from "@/db";
 import * as schema from "@/db/schema";
@@ -10,6 +12,30 @@ export const auth = betterAuth({
     usePlural: true,
     schema,
   }),
+  plugins: [
+    customSession(async ({ user, session }) => {
+      const clinics = await db.query.usersToClinicsTable.findMany({
+        where: eq(schema.usersToClinicsTable.userId, user.id),
+        with: {
+          clinic: true,
+        },
+      });
+
+      // TODO: Ao adaptar para o usuário ter múltiplas clínicas, deve-se mudar essa lógica
+      const clinic = clinics[0];
+
+      return {
+        user: {
+          ...user,
+          clinic: {
+            id: clinic.clinicId,
+            name: clinic.clinic.name,
+          },
+        },
+        session,
+      };
+    }),
+  ],
   user: {
     modelName: "usersTable",
   },
