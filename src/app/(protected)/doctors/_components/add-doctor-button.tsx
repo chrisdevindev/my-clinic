9d5fbc6677f97f51e2bf7f15333b1742bcadd80b
ccastro01 <alvescastro00cca13@gmail.com>
2025-06-11 05:05:21 -0300

    feat: add doctor creation form

---
+++ src/app/(protected)/doctors/_components/add-doctor-button.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Plus } from "lucide-react"; // 1.6k (gzipped: 910)
+
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+
+import UpsertDoctorForm from "./upsert-doctor-form";
+
+const AddDoctorButton = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>
+          <Plus />
+          Adicionar médico
+        </Button>
+      </DialogTrigger>
+      <UpsertDoctorForm />
+    </Dialog>
+  );
+};
+
+export default AddDoctorButton;
